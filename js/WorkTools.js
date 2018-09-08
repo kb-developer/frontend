ac9d960ab16afd09dae22e62bda5adde461cf6c5
Konstantin Bogdanov <web.creator.kot@yandex.ru>
2018-09-08 16:09:47 +0300

    frontend/js #add new WorkTools component

--- js/WorkTools.js
+++ js/WorkTools.js
@@ -1,16 +1,16 @@
 class WorkTools {
     constructor() {}
-    equalHeights(className) {
-        var div = document.getElementsByTagName("div");
-        var tallest = 0;
+    equalHeights(className,el) {
+        let div = document.querySelectorAll(el);
+        let tallest = 0;
         // Loop over matching divs
         for(i = 0; i < div.length; i++)
         {
-            var ele = div[i];
-            var eleHeight = ele.offsetHeight;
-            tallest = (eleHeight>tallest ? eleHeight : tallest); /* look up ternary operator if you dont know what this is */
+            let element = div[i];
+            let elementHeight = element.offsetHeight;
+            tallest = (elementHeight>tallest ? elementHeight : tallest); /* look up ternary operator if you dont know what this is */
         }
-        var findClass = document.getElementsByClassName(className);
+        let findClass = document.getElementsByClassName(className);
         for(i = 1; i < findClass.length; i++)
         {
             findClass[i].style.height = tallest + "px";
