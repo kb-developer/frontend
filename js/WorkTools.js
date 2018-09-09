375b494885380ddae0cfed3fba365d46143b6d02
Konstantin Bogdanov <web.creator.kot@yandex.ru>
2018-09-09 11:21:49 +0300

    frontend/js #refactoring WorkTools component

--- js/WorkTools.js
+++ js/WorkTools.js
@@ -3,12 +3,12 @@ class WorkTools {
     equalHeights(className,el) {
         let col = document.querySelectorAll(el);
         let tallest = 0;
-        // Loop over matching divs
+        
         for(i = 0; i < col.length; i++)
         {
             let element = col[i];
             let elementHeight = element.offsetHeight;
-            tallest = (elementHeight>tallest ? elementHeight : tallest); /* look up ternary operator if you dont know what this is */
+            tallest = (elementHeight>tallest ? elementHeight : tallest);
             element.style.height = tallest;
         }
     }
